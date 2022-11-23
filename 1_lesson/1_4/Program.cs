8e4ce7e7de9ba9abb7a6df0c2c8e6c839a85344d
Alexander Lepilov <lepilow.alexander@mail.ru>
2022-11-24 00:49:54 +0300

    Task 1_4

---
+++ 1_lesson/1_4/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу вычесления модуля числа.
+
+Console.WriteLine("Write a number: ");
+var s = Console.ReadLine();
+int N = s == null ? 0 : int.Parse(s);
+Console.WriteLine($"{Math.Abs(N)}");
