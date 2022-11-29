e2003586782603563717d76966e48461e9b613f6
Alexander Lepilov <lepilow.alexander@mail.ru>
2022-11-29 16:54:32 +0300

    1_2

---
+++ Home_Work/1_2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт,
+// является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Write a number");
+int A = int.Parse(Console.ReadLine());
+
+int B = A % 2;
+
+if (B == 0)
+{
+Console.WriteLine("yes");
+}
+else
+{
+Console.WriteLine("no");
+}
