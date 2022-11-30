2891f4712a3d926e0cfd3aa85b1bb4d2d755193b
Alexander Lepilov <lepilow.alexander@mail.ru>
2022-12-01 00:41:18 +0300

    2_2

--- Home_Work/2_1/Program.cs
+++ Home_Work/2_1/Program.cs
@@ -4,7 +4,7 @@
 Console.WriteLine("Write a number: ");
 string NumberA(int num)
 {
-    if ((-1000 < num && num < -99) || (num < 1000 && num > 99))
+    if ((-1000 < num && num < -100) || (num < 1000 && num > 100))
         return $"{num / 100 * 10 + num % 10}";
     return "Error";
 }
---
+++ Home_Work/2_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая выводит третью цифру
+// заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Write a number: ");
+void NumberA(int num)
+{
+    Console.WriteLine($"{num}");
+    if (num < 100)
+    {
+        Console.WriteLine("num");
+        return;
+    }
+
+    while (num > 999) num /= 10;
+    Console.WriteLine(num % 10);
+}
+NumberA(int.Parse(Console.ReadLine()));
