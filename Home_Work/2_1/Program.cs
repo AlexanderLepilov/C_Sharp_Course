957c310a050c34bf1435a21755616f16809936a3
Alexander Lepilov <lepilow.alexander@mail.ru>
2022-12-01 00:05:57 +0300

    2_1

---
+++ Home_Work/2_1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого числа.
+
+Console.WriteLine("Write a number: ");
+string NumberA(int num)
+{
+    if ((-1000 < num && num < -99) || (num < 1000 && num > 99))
+        return $"{num / 100 * 10 + num % 10}";
+    return "Error";
+}
+Console.Write(NumberA(int.Parse(Console.ReadLine())));
