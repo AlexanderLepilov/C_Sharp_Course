48c71dc00469a9e88dca7da0bd86f8055fae07fa
AlexanderLepilov <lepilow.alexander@mail.ru>
2022-12-14 20:16:03 +0300

    1_3

--- 7_lesson/1_3/Program.cs
+++ 7_lesson/1_3/Program.cs
@@ -1,6 +1,4 @@
-﻿// 2. Задайте двумерный массив размера m на n,
-// каждый элемент в массиве находится по формуле: Aₘₙ = m+n.
-// Выведите полученный массив на экран.
+﻿// 3. Задайте двумерный массив. Найдите сумму элементов главной диагонали.
 
 void Print(int[,] arr)
 {
@@ -17,7 +15,6 @@ void Print(int[,] arr)
 
 }
 
-
 int[,] MassNums(int row, int column)
 {
     int[,] arr = new int[row, column];
@@ -29,10 +26,32 @@ int[,] MassNums(int row, int column)
     return arr;
 }
 
+int Summ(int[,] arr)
+{
+    int sum = 0;
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+            if (i == j)
+            {
+                sum += arr[i, j];
+            }
+
+    }
+
+    return sum;
+
+}
+
 Console.Write("Enter the number of rows: ");
 int row = int.Parse(Console.ReadLine());
 Console.Write("Enter the number of columns: ");
 int column = int.Parse(Console.ReadLine());
 
-int[,] arr_1 = MassNums(row, column);
+int[,] arr_1 = MassNums(row, column,
+                        int.Parse(Console.ReadLine()),
+                        int.Parse(Console.ReadLine()));
 Print(arr_1);
+Console.WriteLine(Summ(arr_1));
