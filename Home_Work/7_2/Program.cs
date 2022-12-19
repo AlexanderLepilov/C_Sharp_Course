2bf675800336560be144606d3d3ac2a95919e33d
AlexanderLepilov <lepilow.alexander@mail.ru>
2022-12-19 16:06:36 +0300

    7_2

--- Home_Work/7_2/Program.cs
+++ Home_Work/7_2/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+            Console.Write($" {arr[i, j]} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+
+}
+
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to);
+    return arr;
+}
+
+void average(int[,] arr)
+{
+    int row = arr.GetLength(0);
+    int colomn = arr.GetLength(1);
+    double res;
+
+    for (int j = 0; j < colomn; j++)
+    {
+        res = 0;
+        for (int i = 0; i < row; i++) res += arr[i, j];
+        Console.Write($"{Math.Round(res / row, 2)}; ");
+    }
+}
+
+Console.Write("Enter the number of rows: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Enter the number of columns: ");
+int column = int.Parse(Console.ReadLine());
+
+int[,] arr_1 = MassNums(row, column,
+                        int.Parse(Console.ReadLine()),
+                        int.Parse(Console.ReadLine()));
+Print(arr_1);
+
+average(arr_1);
