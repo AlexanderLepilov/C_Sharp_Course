8fd1d8aa3408c287921d63d0536efacf76ae73b9
AlexanderLepilov <lepilow.alexander@mail.ru>
2022-12-25 21:59:30 +0300

    9_1

---
+++ Home_Work/9_1/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задайте значения M и N.
+//Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+string NaturChisla(int m, int n)
+{
+    if (m == n)
+        return $"{m} ";
+    return NaturChisla(m, n - 1) + $"{n} ";
+}
+
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(NaturChisla(m, n));
